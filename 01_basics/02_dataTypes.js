9d8de0834e6bf400971c5cc2cc2ba2667614854e
anonymous35-learner <ayushi121104@gmail.com>
2026-05-18 12:14:04 +0000

    Datatypes discussed

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+"use strict" ;//treat all js code as newer version
+// alert(3 + 3) //we do this in browser not in node.js 
+// console.log(3+
+  // 3);  console.log("aaa")
+  // code readability should be high
+
+
+  let name = "Aaaa"
+  let age = 20
+  let isLoggedIn = false
+  let state;
+
+  console.log(typeof "Aaaa");
+  console.log(typeof age);
+  console.log(typeof isLoggedIn );
+  console.log(typeof null);
+  
+  
+  
