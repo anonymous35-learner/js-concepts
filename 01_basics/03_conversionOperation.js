4942b732538841a044f4216ea180c4e5d0ac8ef0
anonymous35-learner <ayushi121104@gmail.com>
2026-05-18 12:29:40 +0000

    Conversion of datatype

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,21 @@
+let score = "33abc"
+
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+
+// console.log(typeof (valueInNumber));
+// console.log(valueInNumber); //prints NaN
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+let sN = 33
+let stringNymber = String(sN)
+
+console.log(stringNymber);
